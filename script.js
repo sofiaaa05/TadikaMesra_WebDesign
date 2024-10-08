ceb6d8cece24dd77b5cfa5d4c8d2d8c41135cf5a
sofiaaa05 <sofianugraheni05@gmail.com>
2024-10-08 11:02:24 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const loginBtn = document.querySelector("#login");
+const registerBtn = document.querySelector("#register");
+const loginform= document.querySelector("#login-form");
+const registerform = document.querySelector("#register-form");
+
+loginBtn.addEventListener('click', () => {
+    loginBtn.style.backgroundColor = "#21264D;
+    registerBtn.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
+
+loginForm.style.left = "50%";
+registerForm.style.left = "-50%";
+
+loginForm.style.opacity= 1;
+registerForm.style.opacity = 0;
+})
+
+registerBtn.addEventListener('click', () => {
+    loginBtn.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
+    registerBtn.style.backgroundColor = "21264D";
+
+loginForm.style.left = "150%";
+registerForm.style.left = "-50%";
+
+loginForm.style.opacity= 1;
+registerForm.style.opacity = 0;
+})
