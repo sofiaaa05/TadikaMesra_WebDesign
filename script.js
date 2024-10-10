97cdfda18cc4f811cf3bea6249be931dc425edb8
sofiaaa05 <sofianugraheni05@gmail.com>
2024-10-10 12:14:22 +0700

    helo

--- script.js
+++ script.js
@@ -14,6 +14,16 @@ loginForm.style.opacity= 1;
 registerForm.style.opacity = 0;
 })
 
+async function handleSubmit() {
+    try {
+        const response = await fetch ('url');
+    } catch (error){
+        console.error('Error:', error);
+    }
+}
+
+document.querySelector('#loginSubmit').addEventListener('click, handlSubmit');
+
 registerBtn.addEventListener('click', () => {
     loginBtn.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
     registerBtn.style.backgroundColor = "21264D";
